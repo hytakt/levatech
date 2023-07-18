d5ad8e662370bef64d19e7b8e180ad44d18b9758
hytakt <hayate.akutsu04202002@gmail.com>
2023-07-19 01:10:48 +0900

    Blogのh1、pだけ

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Blog</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+       
+    </head>
+    <body >
+        <h1>Blog Name</h1>
+        <div class='posts'>
+            <div class='post'>
+                <h2>Title</h2>
+                <p class='body'>This is a sample body.</p>
+            </div>
+        </div>
+    </body>
+</html>
