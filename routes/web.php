d50b933a35338ce206636e0c33971f1188ed594f
hytakt <hayate.akutsu04202002@gmail.com>
2023-07-19 13:34:04 +0900

    Add pagiutation02

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+
 use App\Http\Controllers\PostController;
 
 /*
@@ -14,4 +15,10 @@ use App\Http\Controllers\PostController;
 |
 */
 
-Route::get('/',[PostController::class,'index']);
+Route::get('/', function () {
+    return view('posts.index');
+});
+
+Route::get('/posts', [PostController::class, 'index']);
+
+Route::get('/posts/{post}', [PostController::class ,'show']);
