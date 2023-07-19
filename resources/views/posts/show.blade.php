bb3922a3a4a4d8c9ade2937e7a93fda69235c7d9
hytakt <hayate.akutsu04202002@gmail.com>
2023-07-19 13:16:55 +0900

    Addpagination

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -2,16 +2,23 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
         <title>Blog</title>
+
         <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+       
     </head>
-    <body>
-        <h1>Blog Name</h1>
+    <body >
+        <h1 class='title'>
+            {{ $post->title }}
+        </h1>
         <div class='posts'>
             @foreach ($posts as $post)
                 <div class='post'>
-                    <h2 class='title'>{{ $post->title }}</h2>
+                    <h2>{{ $post->title }}</h2>
                     <p class='body'>{{ $post->body }}</p>
                 </div>
             @endforeach
