14c6e67775f2405faff0d170e550c4baa48f3ab6
hytakt <hayate.akutsu04202002@gmail.com>
2023-07-24 23:12:03 +0900

    dev_basis03

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -2,29 +2,22 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-
         <title>Blog</title>
-
         <!-- Fonts -->
-        <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
-
-       
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     </head>
-    <body >
+    <body class='antialiased'>
         <h1 class='title'>
-            {{ $post->title }}
+            {{$post->title }}
         </h1>
-        <div class='posts'>
-            @foreach ($posts as $post)
-                <div class='post'>
-                    <h2>{{ $post->title }}</h2>
-                    <p class='body'>{{ $post->body }}</p>
-                </div>
-            @endforeach
+        <div class='content'>
+            <div class='content_post'>
+                <h3>本文</h3>
+                <p class='body'>{{ $post->body }}</p>
+            </div>
         </div>
-        <div class='paginate'>
-            {{ $posts->links() }}
+        <div class='footer'>
+            <a href='/'>戻る</a>
         </div>
     </body>
 </html>
