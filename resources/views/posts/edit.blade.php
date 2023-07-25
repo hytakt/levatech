0a306229215a88dcf87d6689df9f3d40df0a0533
hytakt <hayate.akutsu04202002@gmail.com>
2023-07-25 22:55:17 +0900

    ADD投稿画面

---
+++ resources/views/posts/edit.blade.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <title>Blog</title>
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+    </head>
+    <body class='antialiased'>
+        <h1>Blog Name</h1>
+        <form action="/posts/{{ $post->id }}" method="POST">
+            @csrf
+            @method('PUT')
+            <div class="title">
+                <h2>Title</h2>
+                <input type="text" name="post[title]" placeholder="タイトル" value={{ $post->title }}>
+            </div>
+            <div class="body">
+                <h2>Body</h2>
+                <textarea name="post[body]" placeholder="今日も一日お疲れ様でした。">{{ $post->body }}</textarea>
+            </div>
+            <input type="submit" value="store">
+        </form>
+        <div class='footer'>
+            <a href='/'>戻る</a>
+        </div>
+    </body>
+</html>
